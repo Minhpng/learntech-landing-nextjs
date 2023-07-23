d239671a0d534961e0d74ec268d100f3b4f2fdb8
minhpng <nguyenminh.236@gmail.com>
2023-07-23 22:21:30 +0700

    add

--- app/payment/layout.js
+++ app/payment/layout.js
@@ -1,46 +1,46 @@
-import styles from './payment.module.css'
+import './payment.css'
 
 const supportTel = '097 397 9109'
 
 
 function PaymentLayout({ children }) {
     return (<>
-        <section className={`${styles.paymentHero} bg-dark`}>
+        <section className='paymentHero bg-dark'>
             <div className="container">
-                <h2 className={styles.paymentHeader}>THANH TOÁN</h2>
+                <h2 className='paymentHeader'>THANH TOÁN</h2>
                 <div className="logo">
                     <img src="/logo.png" alt="Learntech logo" />
                 </div>
-                <h2 className={styles.paymentSubtitle}>Kênh phân phối chính thức của <span className='inline'>Lang Kingdom</span> tại thị trường Việt Nam</h2>
-                <h3 className={styles.paymentSubtitle}>An toàn - Bảo mật - Nhanh chóng</h3>
+                <h2 className='paymentSubtitle'>Kênh phân phối chính thức của <span className='inline'>Lang Kingdom</span> tại thị trường Việt Nam</h2>
+                <h3 className='paymentSubtitle'>An toàn - Bảo mật - Nhanh chóng</h3>
             </div>
         </section>
         <section className='section'>
             <div className='container'>
-                <div className={styles.paymentWrapper}>
+                <div className='paymentWrapper'>
                     {children}
-                    <div className={styles.paymentSupportWrapper}>
-                        <p className={styles.paymentInfoHeading}>Nếu bạn có thắc mắc vui lòng CHAT hoặc GỌI cho chúng tôi để được giải đáp</p>
-                        <div className={styles.supportListWrapper}>
-                            <div className={styles.paymentSupportImage}>
+                    <div className='paymentSupportWrapper'>
+                        <p className='paymentInfoHeading'>Nếu bạn có thắc mắc vui lòng CHAT hoặc GỌI cho chúng tôi để được giải đáp</p>
+                        <div className='supportListWrapper'>
+                            <div className='paymentSupportImage'>
                                 <img src="/chat.svg"></img>
                             </div>
                             <a href="">Nhắn tin Messenger cho chúng tôi</a>
                         </div>
-                        <div className={styles.supportListWrapper}>
-                            <div className={styles.paymentSupportImage}>
+                        <div className='supportListWrapper'>
+                            <div className='paymentSupportImage'>
                                 <img src="/call.svg"></img>
                             </div>
                             <a href="">Hotline: {supportTel}</a>
                         </div>
-                        <div className={styles.supportListWrapper}>
-                            <div className={styles.paymentSupportImage}>
+                        <div className='supportListWrapper'>
+                            <div className='paymentSupportImage'>
                                 <img src="/icon-zalo.svg"></img>
                             </div>
                             <a href="">Zalo chat: {supportTel}</a>
                         </div>
-                        <div className={styles.supportListWrapper}>
-                            <div className={styles.paymentSupportImage}>
+                        <div className='supportListWrapper'>
+                            <div className='paymentSupportImage'>
                                 <img src="/mail.svg"></img>
                             </div>
                             <a href="">Email: support@langkingdom.com</a>
--- app/payment/page.js
+++ app/payment/page.js
@@ -1,8 +1,7 @@
 'use client'
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
-import styles from './payment.module.css'
-import { useRouter } from 'next/router';
+import './payment.css'
 import Link from 'next/link';
 
 const paymentMethods = {
@@ -16,40 +15,51 @@ const paymentMethods = {
 function PaymentPage() {
     const [paymentMethod, setMethod] = useState(paymentMethods.transfer)
     const [isVideoOpened, setOpenVideo] = useState(false)
+    const [promoCode, setPromoCode] = useState('')
 
     function handlePaymentMethod(e) {
         const method = e.target.value
         setMethod(() => method)
     }
 
-    function openVideo() {
+    const openVideo = useCallback(() => {
         setOpenVideo(!isVideoOpened)
-    }
+    }, [isVideoOpened])
+
+    const onApplyPromoCode = useCallback((e) => {
+        e.preventDefault();
+        console.log(promoCode)
+    })
+
     return <>
-        <form className={styles.paymentFormWrapper}>
-            <div className={styles.userPaymentInfoWrapper}>
-                <h3 className={styles.paymentInfoHeading}>Thông tin thanh toán</h3>
-                <div className={styles.userInfoItem}>
-                    <label htmlFor="">Họ và tên</label>
+        <form className='paymentFormWrapper'>
+            <div className='userPaymentInfoWrapper'>
+                <h3 className='paymentInfoHeading'>Thông tin thanh toán</h3>
+                <div className='userInfoItem'>
+                    <label htmlFor="user_name">Họ và tên*</label>
                     <input type="text" name="user_name" placeholder="Họ và tên" autoComplete='given-name' required />
+                    <span className='body-text text-red'>Vui lòng nhập thông tin này</span>
                 </div>
-                <div className={styles.userInfoItem}>
-                    <label htmlFor="">Số điện thoại</label>
+                <div className='userInfoItem'>
+                    <label htmlFor="user_phone">Số điện thoại*</label>
                     <input type="text" name="user_phone" placeholder="Số điện thoại" autoComplete='tel' required />
+                    <span className='body-text text-red'>Vui lòng nhập thông tin này</span>
                 </div>
-                <div className={styles.userInfoItem}>
-                    <label htmlFor="">Địa chỉ email</label>
+                <div className='userInfoItem'>
+                    <label htmlFor="">Địa chỉ email*</label>
                     <input type="text" name="user_email" placeholder="Email" autoComplete='email' required />
+                    <span className='body-text text-red'>Vui lòng nhập thông tin này</span>
                 </div>
-                <div className={styles.userInfoItem}>
-                    <label htmlFor="">ID tài khoản Lang Kingdom</label>
+                <div className='userInfoItem'>
+                    <label htmlFor="user_id">ID tài khoản Lang Kingdom*</label>
                     <input type="text" name="user_id" placeholder="ID Lang Kingdom" autoComplete='none' required />
-                    <p onClick={openVideo} className={styles.userInfoExplain}>Xem video hướng dẫn lấy id {<i className={`fa-solid ${!isVideoOpened ? "fa-circle-chevron-down" : "fa-circle-chevron-up"}`} />}</p>
+                    <span className='body-text text-red'>Vui lòng nhập thông tin này</span>
+                    <p onClick={openVideo} className='userInfoExplain'>Xem video hướng dẫn lấy id {<i className={`fa-solid ${!isVideoOpened ? "fa-circle-chevron-down" : "fa-circle-chevron-up"}`} />}</p>
                     {
-                        isVideoOpened ? <div className={styles.paymentVideoWrapper}>
-                            <video controls poster=''>
+                        isVideoOpened ? <div className='paymentVideoWrapper'>
+                            <video controls>
                                 <source
-                                    src="https://static.langkingdom.com/webinars/videos/LP-hero-220814.mp4"
+                                    src="https://static.langkingdom.com/webinars/videos/huong_dan_offer.mp4"
                                     type="video/mp4"
                                 />
                             </video>
@@ -58,55 +68,66 @@ function PaymentPage() {
                 </div>
             </div>
 
-            <div className={styles.paymentDivider}></div>
+            <div className='paymentDivider'></div>
 
 
-            <div className={styles.billingDetailWrapper}>
-                <p className={styles.paymentInfoHeading}>Đơn hàng của bạn</p>
-                <div className={styles.billingUserInfo}>
-                    <p className={styles.billingItemMainText}>Tài khoản Lang Kingdom:</p>
-                    <p className={styles.billingUserAccount}>Minhphamxxx</p>
+            <div className='billingDetailWrapper'>
+                <p className='paymentInfoHeading'>Đơn hàng của bạn</p>
+                <div className='billingUserInfo'>
+                    <p className='billingItemMainText'>Tài khoản Lang Kingdom:</p>
+                    <p className='billingUserAccount'>Minhphamxxx</p>
                 </div>
-                <div className={styles.billingProductItem}>
-                    <p className={styles.billingItemMainText}>
+                <div className='billingProductItem'>
+                    <p className='billingItemMainText'>
                         Sản phẩm
                     </p>
-                    <p className={styles.billingItemMainText}>Số tiền</p>
+                    <p className='billingItemMainText'>Số tiền</p>
                 </div>
-                <div className={styles.billingProductItem}>
+                <div className='billingProductItem'>
                     <p>Lang kingdom Pro 1 năm</p>
-                    <p className={styles.billingItemMainText}>1,490,000đ</p>
+                    <p className='billingItemMainText'>1,490,000đ</p>
                 </div>
-                <div className={styles.billingProductItem}>
-                    <p>Mã giảm giá: <span className={styles.billingUserAccount}>XXXX</span> <span style={{ color: "red", fontWeight: "700" }}><i className="fa-solid fa-circle-xmark"></i> Xóa</span></p>
-                    <p className={styles.billingItemMainText}> -50,000đ</p>
+                <div className='billingProductItem'>
+                    <div>
+                        <p>Mã giảm giá:</p>
+                        <p>
+                            <span className='billingUserAccount'>XXXX</span> <span style={{ color: "red", fontWeight: "700", cursor: 'pointer' }}><i className="fa-solid fa-circle-xmark"></i> Xóa</span>
+                        </p>
+                    </div>
+                    <p className='billingItemMainText'> -50,000đ</p>
 
                 </div>
-                <div className={styles.paymentDivider}></div>
+                <div className='paymentDivider'></div>
 
-                <div className={styles.billingProductItem}>
-                    <p className={styles.billingItemMainText}>
+                <div className='billingProductItem'>
+                    <p className='billingItemMainText'>
                         Tổng
                     </p>
-                    <p className={styles.billingTotalPrice}>1,440,000đ</p>
+                    <p className='billingTotalPrice'>1,440,000đ</p>
                 </div>
 
-                <div className={styles.paymentDiscount}>
-                    <div className={styles.discountInputWrapper}>
-                        <input type="text" name='discount' id='discount' placeholder='Mã giảm giá' ></input>
-                        <button>Áp dụng</button>
+                <div className='paymentDiscount'>
+                    <div className='discountInputWrapper'>
+                        <input
+                            type="text"
+                            name='discount'
+                            id='discount'
+                            value={promoCode}
+                            onChange={(e) => setPromoCode(e.target.value)}
+                            placeholder='Mã giảm giá' ></input>
+                        <button onClick={(e) => onApplyPromoCode(e)}>Áp dụng</button>
                     </div>
-                    <span className={styles.discountErrorText}>Mã này không hợp lệ. Bạn vui lòng kiểm tra lại nhé.</span>
+                    <span className='discountErrorText'>Mã này không hợp lệ. Bạn vui lòng kiểm tra lại nhé.</span>
                 </div>
 
             </div>
 
-            <div className={styles.paymentDivider}></div>
+            <div className='paymentDivider'></div>
 
-            <div className={styles.paymentMethodsWrapper}>
-                <h3 className={styles.paymentSubHeading}>Chọn hình thức thanh toán:</h3>
-                <div className={styles.paymentMethodItem}>
-                    <div className={styles.methodItemOption}>
+            <div className='paymentMethodsWrapper'>
+                <h3 className='paymentSubHeading'>Chọn hình thức thanh toán:</h3>
+                <div className='paymentMethodItem'>
+                    <div className='methodItemOption'>
                         <input
                             type="radio"
                             name='payment-method'
@@ -119,15 +140,15 @@ function PaymentPage() {
                         <label htmlFor="chuyen_khoan">Chuyển khoản hoặc nạp tiền trực tiếp vào tài khoản Ngân hàng</label>
                     </div>
                     {paymentMethod === paymentMethods.transfer ?
-                        <div className={styles.methodItemDesc}>
+                        <div className='methodItemDesc'>
                             <p>Bấm nút <span className='text-org font-weight-700'>Tiến hành thanh</span> toán để xem tài khoản ngân hàng và làm theo hướng dẫn trên màn hình.</p>
                         </div>
                         : null
                     }
 
                 </div>
-                <div className={styles.paymentMethodItem}>
-                    <div className={styles.methodItemOption}>
+                <div className='paymentMethodItem'>
+                    <div className='methodItemOption'>
                         <input
                             type="radio"
                             name='payment-method'
@@ -139,15 +160,15 @@ function PaymentPage() {
                         <label htmlFor="internet_banking">Thẻ ATM đăng ký Internet Banking</label>
                     </div>
                     {paymentMethod === paymentMethods.internetBanking ?
-                        <div className={styles.methodItemDesc}>
+                        <div className='methodItemDesc'>
                             <ul style={{ paddingLeft: '20px' }}>
                                 <li>Bạn sẽ được chuyển sang cổng thanh toán <span className='text-org font-weight-700'>Bảo Kim</span> nhằm đảm bảo an toàn cho quá trình thanh toán của bạn.</li>
                                 <li>Để thực hiện thanh toán, hãy chắc chắn rằng <span className='text-org font-weight-700'>thẻ ATM</span> của bạn đã có đăng ký sử dụng dịch vụ <span className='text-org font-weight-700'>Internet Banking</span>.</li>
                             </ul>
                         </div> : null}
                 </div>
-                <div className={styles.paymentMethodItem}>
-                    <div className={styles.methodItemOption}>
+                <div className='paymentMethodItem'>
+                    <div className='methodItemOption'>
                         <input
                             type="radio"
                             name='payment-method'
@@ -159,12 +180,12 @@ function PaymentPage() {
                         <label htmlFor="visa">Thẻ Visa, Master phát hành tại Việt Nam</label>
                     </div>
                     {paymentMethod === paymentMethods.visa ?
-                        <div className={styles.methodItemDesc}>
+                        <div className='methodItemDesc'>
                             <p>Bạn sẽ được chuyển sang cổng thanh toán <span className='text-org font-weight-700'>Bảo Kim</span> nhằm đảm bảo an toàn cho quá trình thanh toán của bạn.</p>
                         </div> : null}
                 </div>
-                <div className={styles.paymentMethodItem}>
-                    <div className={styles.methodItemOption}>
+                <div className='paymentMethodItem'>
+                    <div className='methodItemOption'>
                         <input
                             type="radio"
                             name='payment-method'
@@ -176,7 +197,7 @@ function PaymentPage() {
                         <label htmlFor="paypal">Paypal (chấp nhận tất cả thẻ Visa, Master trong và ngoài nước)</label>
                     </div>
                     {paymentMethod === paymentMethods.paypal ?
-                        <div className={styles.methodItemDesc}>
+                        <div className='methodItemDesc'>
                             <p>Qua cổng thanh toán quốc tế PayPal, bạn có thể thanh toán bằng một trong các hình thức sau:</p>
                             <ul style={{ paddingLeft: '20px' }}>
                                 <li>
@@ -188,7 +209,7 @@ function PaymentPage() {
                         </div> : null}
                 </div>
             </div>
-            <Link href={`/payment/payment-transfer`} className={styles.processPaymentBtn}>TIẾN HÀNH THANH TOÁN<span>1,440,000đ</span></Link>
+            <Link href={`/payment/transfer`} className='processPaymentBtn'>TIẾN HÀNH THANH TOÁN<span>1,440,000đ</span></Link>
         </form>
     </>;
 }
